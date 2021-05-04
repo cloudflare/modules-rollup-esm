0438a11b1eb9859c61b79f2dd5a3323784140a4c
Piyush Kalsariya <piyushkalasariya555@gmail.com>
2021-05-04 11:53:20 +0530

    Reproducing jsonwebtoken issue

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,4 @@
+import { Secret, sign, verify as verifyCb, VerifyOptions } from 'jsonwebtoken'
 import slug from 'slug.txt'
 
 export default {
@@ -11,5 +12,15 @@ export default {
 }
 
 async function handleRequest(request, env) {
+  const token = sign(
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+    'secret',
+    {
+      expiresIn: '24h',
+    },
+  )
+
+  console.log(token)
+
   return new Response(slug)
 }
