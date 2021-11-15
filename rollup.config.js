46314b1f82328298b2f6811b487a4f18331d7dd6
Sanat Gersappa <sanat.gersappa@gmail.com>
2021-11-15 20:30:22 +0530

    Add Rollup plugin-json

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
-import { terser } from 'rollup-plugin-terser'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import copy from 'rollup-plugin-copy'
+import commonjs from '@rollup/plugin-commonjs';
+import json from "@rollup/plugin-json";
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import copy from 'rollup-plugin-copy';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/index.mjs',
@@ -19,6 +20,7 @@ export default {
     commonjs(),
     nodeResolve({ browser: true }),
     terser(),
+    json({ compact: true }),
     copy({
       targets: [{ src: './src/slug.txt', dest: './dist/' }],
     }),
